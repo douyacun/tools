75c5d9f0d837e3b4565033f8997d0e39ec742bb7
liuning <douyacun@gmail.com>
2021-01-27 16:00:52 +0800

    etcd

--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 
 - [ip2location](https://lite.ip2location.com/database/ip-country-region-city-latitude-longitude-zipcode-timezone)
 - [全球城市地区](./db/global_region.csv)
+
+## item2
+
+- [lrzsz](https://github.com/aikuyun/iterm2-zmodem) 跳板机下载工具
---
+++ docker/etcd/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#!/bin/bash
+nohup etcd --name=default --data-dir=/Users/admin/Documents/tools/docker/etcd/data --listen-client-urls=http://0.0.0.0:2379 --advertise-client-urls=http://0.0.0.0:2379 --auto-compaction-retention=1 >> access.log 2>&1 &
